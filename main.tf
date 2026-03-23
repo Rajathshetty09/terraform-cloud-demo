4c42c54c8fac6fcd5da414e86afc9cce7773be19
Sonal Mittal <62133890+Sonal0409@users.noreply.github.com>
2026-03-23 16:13:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,49 +3,5 @@ region = "us-east-1"
 
 }
 
-data "aws_ami" "myami" {
-
-
-
-  most_recent = true
-
-
-
-  owners = ["amazon"]
-
-
-
-  filter{
-
-    name = "name"
-
-    values = ["amzn2-ami-hvm*"]
-
-}
-
-
-
-}
-
-resource "aws_instance" "test-ec2" {
-
-
-
-ami = "ami-02dfbd4ff395f2a1b"
-
-
-
-instance_type = "t2.micro"
-
-  tags = {
-
-    Name = "Instance-terraform"
-
-  }
-
-
-}
-
-
 
 
